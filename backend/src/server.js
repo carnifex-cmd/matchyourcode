f0ebfa4e3ffd2451f6e5aec919ce9bea83c790ca
Shardul Sawant <shardul855@gmail.com>
2025-03-16 19:19:09 +0530

    Fixing CORs error

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,9 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: '*',
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
